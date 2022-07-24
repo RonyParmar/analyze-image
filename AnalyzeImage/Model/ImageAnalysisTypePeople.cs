0943efd504acb10c59b85066a7912829a5183e18
RonyParmar <ronyparm@buffalo.edu>
2022-07-24 19:55:14 -0400

    initial project implementation

---
+++ AnalyzeImage/Model/ImageAnalysisTypePeople.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace RonyParm.Azure.AnalyzeImage
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    public class ImageAnalysisTypePeople
+    {
+        [JsonProperty(Required = Required.Always)]
+        public string id;
+
+        [JsonProperty(Required = Required.Always)]
+        public string blobUrl;
+
+        [JsonProperty(Required = Required.Always)]
+        public int totalPeople;
+
+        [JsonProperty(Required = Required.Always)]
+        public string type;
+
+        [JsonProperty(Required = Required.Always)]
+        public DateTime createdDateTime;
+    }
+}
---
+++ AnalyzeImage/Startup/Startup.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Extensions.DependencyInjection;
+using RonyParm.Azure.AnalyzeImage;
+using Azure.Storage.Blobs;
+using System;
+using Microsoft.Azure.Cosmos;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+
+namespace RonyParm.Azure.AnalyzeImage
+{
+    /// <summary>
+    /// Register services as singletons and utilize constructor based dependency injection
+    /// https://docs.microsoft.com/en-us/azure/azure-functions/functions-dotnet-dependency-injection
+    /// </summary>
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddSingleton((s) => {
+                string cosmosConnectionString = Environment.GetEnvironmentVariable("CosmosConnectionString");
+                return new CosmosClient(cosmosConnectionString);
+            });
+
+            builder.Services.AddSingleton((s) => {
+                string blobConnectionString = Environment.GetEnvironmentVariable("BlobConnectionString");
+                return new BlobServiceClient(blobConnectionString);
+            });
+
+            builder.Services.AddSingleton((s) => {
+                string computerVisionKey = Environment.GetEnvironmentVariable("ComputerVisionKey");
+                string computerVisionEndpoint = Environment.GetEnvironmentVariable("ComputerVisionEndpoint");
+                return new ComputerVisionClient(new ApiKeyServiceClientCredentials(computerVisionKey))
+                {
+                    Endpoint = computerVisionEndpoint
+                };
+            });
+        }
+    }
+}
